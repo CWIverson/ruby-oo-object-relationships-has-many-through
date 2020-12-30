b422bcdfde7914e618941e55d226c2668430b4aa
christian iverson <chris.w.iverson@gmail.com>
2020-12-29 17:42:32 -0800

    commit tree 658eaaababbb2d4f27372d94ea689961db670703

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,24 @@
 class Waiter
-
+    @@all=[]
+    def initialize(name, experience)
+        @name= name
+        @experience= experience
+        @@all<<self
+    end
+    def self.all
+        @@all
+    end
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+    def best_tipper
+        ranking = meals.sort_by{|meal| meal.tip}
+        winner=ranking[-1]
+        winner.customer
+    end
 end
